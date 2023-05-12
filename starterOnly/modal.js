88f9cc3f547d16b87be1ba581d5315339d5da2d6
Kader-occ <digiform92@gmail.com>
2023-05-12 09:17:45 +0200

    Fermeture modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,4 +20,16 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//Fermer le modal event
+closeBtn.addEventListener('click', closeModal)
+
+//Fermer le modal form
+function closeModal() {
+  //modalbg.style.animation = "modalclose"
+  modalbg.style.display = "none"
+}
+
+//Valider le formulaire d'inscription
+function validate() { }
 
