be9e7abd5afec979ce93e8504f60e47533bac6b8
Kader-occ <digiform92@gmail.com>
2023-05-12 09:14:08 +0200

    Projet initial

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,15 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Fermer le modal event
+closeBtn.addEventListener('click', closeModal)
+
+//Fermer le modal form
+function closeModal() {
+  //modalbg.style.animation = "modalclose"
+  modalbg.style.display = "none"
+}
+
+//Valider le formulaire d'inscription
+function validate() { }
 
